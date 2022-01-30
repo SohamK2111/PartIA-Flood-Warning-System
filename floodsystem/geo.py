0bd3f7dcbe34f23a53d311bc2f54a8ca5f715608
Soham Karmarkar <soham.karmarkar@yahoo.com>
2022-01-30 19:46:17 +0000

    reset commit

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,14 +7,3 @@ geographical data.
 """
 
 from utils import sorted_by_key  # noqa
-from stationdata import build_station_list
-from haversine import haversine, Unit
-
-stations = build_station_list()
-
-stations_and_distance = []
-
-def stations_by_distance(stations, p):
-    for station in stations:
-        stations_and_distance.append((station, haversine(p, station.coord)))
-    return sorted_by_key(stations_and_distance, 1)
