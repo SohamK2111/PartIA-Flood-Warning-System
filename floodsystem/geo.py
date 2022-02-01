cfaf737ee2a45be6c9e0a6615cc12069124994f5
Soham Karmarkar <soham.karmarkar@yahoo.com>
2022-02-01 22:26:13 +0000

    Task 1B

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,8 +5,8 @@
 geographical data.
 
 """
-
-from utils import sorted_by_key  # noqa
+from floodsystem.stationdata import build_station_list
+from floodsystem.utils import sorted_by_key  # noqa
 from haversine import haversine, Unit
 from floodsystem.stationdata import build_station_list
 
@@ -20,6 +20,16 @@ def stations_by_distance(stations, p):
         stations_and_distance.append((station, haversine(p, station.coord)))
     return sorted_by_key(stations_and_distance, 1)
 
-print(stations[0])
-print("Hi Ella")
 
+def task_1B():
+   a = stations_by_distance(stations, (52.2053, 0.1218))
+   print("The 10 CLOSEST stations are: ")
+   for i in range(10):
+      print(str(a[i][0].name) + ", " + str(a[i][0].town) + ", " + str(a[i][1]))
+
+   print("\n The 10 FURTHEST stations are: ")
+   for i in range(10):
+      print(str(a[-i][0].name) + ", " + str(a[-i][0].town) + ", " + str(a[-i][1]))
+
+
+task_1B()
