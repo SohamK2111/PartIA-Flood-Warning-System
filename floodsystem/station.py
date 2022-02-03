afc49de5dafc59f9e955b334653e4058a1da77e3
Soham Karmarkar <soham.karmarkar@yahoo.com>
2022-02-03 15:07:29 +0000

    Task 1F Part 1

--- floodsystem/station.py
+++ floodsystem/station.py
@@ -38,3 +38,12 @@ class MonitoringStation:
         d += "   river:         {}\n".format(self.river)
         d += "   typical range: {}".format(self.typical_range)
         return d
+    
+    def typical_range_consistent(self):
+        if self.typical_range[1] > self.typical_range[0]:
+            return True
+        else:
+            return False
+    
+
+
