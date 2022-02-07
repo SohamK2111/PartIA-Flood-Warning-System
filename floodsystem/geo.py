5d199d2fbd3761d057251e7ac3f3e8702d0b8ca3
Soham Karmarkar <soham.karmarkar@yahoo.com>
2022-02-07 18:54:13 +0000

    Amended geo file after commit issues

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -19,52 +19,63 @@ def stations_by_distance(stations, p):
 
    """This function, given a list of staiton objects and a coordinate p, returns a list of (station, distance) tuples, 
    where "distance" is the distance of the station from the coordinate p. This list is also sorted in order of distance."""
+   
    stations_and_distance = []
    for station in stations:
-        d = haversine(centre, station.coord)
-        if d < r or d == r:
-            stations_in_radius.append(station)
+      stations_and_distance.append((station.name, haversine(p, station.coord)))
+   return sorted_by_key(stations_and_distance, 1)
 
+def stations_within_radius(stations, centre, r):
+
+   """This function given a list of station objects and a coordinate centre, returns a list of station names that are 
+   within radius r"""
+
+   stations_in_radius = []
+
+   for station in stations:
+      d = haversine(centre, station.coord)
+      if d < r or d == r:
+         stations_in_radius.append(station)
+   
    list_of_names = []
    for station in stations_in_radius:
-        list_of_names.append(station.name)
-    
+      list_of_names.append(station.name)
    print(sorted(list_of_names))
-   sorted_list_of_names = sorted(list_of_names)
-   return sorted_list_of_names
+   return(sorted(list_of_names))
 
 
 def rivers_by_station_number(stations, N):
-    #create list of all river names
-    rivers = []
+   """This function creates a list of rivers with the most stations. This list is ordered. It returns the fors N number 
+   of stations. It takes in arguments: stations, a list of all the stations in the database, and N, the number of stations 
+   to be printed. It returns a list of tuples with the river name and the number of stations."""
+
+   rivers = []
     
-    for station in stations:
+   for station in stations:
         if station.river not in rivers:
             rivers.append(station.river)
     
-    list_of_tuples = []
+   list_of_tuples = []
 
-    for river in rivers:
-        counter = 0
-        for station in stations:
-            if station.river == river:
-                counter += 1 
-        list_of_tuples.append((river, counter))
+   for river in rivers:
+      counter = 0
+      for station in stations:
+         if station.river == river:
+            counter += 1 
+      list_of_tuples.append((river, counter))
     
-    sorted_list = sorted_by_key(list_of_tuples, 1, True)
+   sorted_list = sorted_by_key(list_of_tuples, 1, True)
 
-    #TODO - in the case that there are more rivers with same number of stations, print them too.
-    #number of items in list excluding first N
-    x = len(sorted_list) - N
-    counter2 = 0
+   x = len(sorted_list) - N
+   counter2 = 0
 
-    for item in sorted_list[-x:]:
-        if item[1] == sorted_list[N][1]:
-            counter2 += 1
-        else:
-            break
+   for item in sorted_list[-x:]:
+      if item[1] == sorted_list[N][1]:
+         counter2 += 1
+      else:
+         break
     
-    print(sorted_list[:N+counter2])
+   print(sorted_list[:N+counter2])
 
 
 def rivers_with_station(stations):
