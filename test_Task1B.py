791e763df5c3c23031f6d38e85e95652d4c3b1e3
Soham Karmarkar <soham.karmarkar@yahoo.com>
2022-02-09 23:37:39 +0000

    Test 1B fix

--- test_Task1B.py
+++ test_Task1B.py
@@ -1,6 +1,7 @@
 from floodsystem.geo import *
 
 def test_stations_by_distance():
-    for i in range(len(stations_and_distance) - 1):
-        assert stations_and_distance[i][1] < stations_and_distance[i+1][1]
+    for i in range(len(stations_by_distance) - 1):
+        assert stations_by_distance[i][1] < stations_by_distance[i+1][1]
+
 
