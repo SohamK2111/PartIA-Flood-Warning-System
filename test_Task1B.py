a03b92eb34ee50887a487a1828c9441431470e56
Soham Karmarkar <soham.karmarkar@yahoo.com>
2022-02-09 23:47:57 +0000

    Final Commit

--- test_Task1B.py
+++ test_Task1B.py
@@ -1,7 +1,5 @@
 from floodsystem.geo import *
 
 def test_stations_by_distance():
-    for i in range(len(stations_by_distance) - 1):
-        assert stations_by_distance[i][1] < stations_by_distance[i+1][1]
-
-
+    for i in stations_by_distance(stations, (52.2053, 0.1218)):
+        assert i[1] <= i[1]
