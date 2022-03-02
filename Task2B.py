ec4895b8e3d4da5f80e9f3c37f14b7f6110436dc
ella-oc16 <eo378@cam.ac.uk>
2022-03-02 02:34:14 +0000

    Task2B

--- Task2B.py
+++ Task2B.py
@@ -1,9 +1,10 @@
 from floodsystem.flood import stations_level_over_threshold
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 
 def run():
 
     stations = build_station_list()
+    update_water_levels(stations)
     a = stations_level_over_threshold(stations, 0.8)
     for i in a:
         print(i[0], i[1])
